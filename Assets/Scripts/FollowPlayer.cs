7d5a20a71fbe38965f581a7464cec90e65611101
Hui Wang <huicnvictor@gmail.com>
2024-06-13 00:50:42 +0200

    Camera is finished
    Player can move but no animation
    Player goes down under the road, because of different of the terrian`s road

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + new Vector3(0,3,-4);
+    }
+}
