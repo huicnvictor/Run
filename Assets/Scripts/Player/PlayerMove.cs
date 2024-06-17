f12f3f8eb97efb8d103b63a71eaba60c1c19acc5
Hui Wang <huicnvictor@gmail.com>
2024-06-17 14:39:19 +0200

    Delete the useless assets

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -10,24 +10,15 @@ public class PlayerMove : MonoBehaviour
     public bool isJumping = false;
     public bool comingDown = false;
     public GameObject playerObject;
-    // public float horizontalInput;
 
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
 
         //Move the player forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.World);
 
-        // horizontalInput = Input.GetAxis("Horizontal");
-        //Rotates the player based on horizontal input
-        //   transform.Translate(Vector3.left * Time.deltaTime * turnSpeed * horizontalInput);
+
 
         //Keep player in the Boundary
         if (canMove == true)
@@ -35,14 +26,16 @@ public class PlayerMove : MonoBehaviour
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
             {
                 if (this.gameObject.transform.position.x > LevelBoundary.leftside)
-                { transform.Translate(Vector3.left * Time.deltaTime * turnSpeed); }
+                { transform.Translate(Vector3.left * Time.deltaTime * turnSpeed); 
+                }
             }
             if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             {
                 if (this.gameObject.transform.position.x < LevelBoundary.rightside)
-                { transform.Translate(Vector3.left * Time.deltaTime * turnSpeed * -1); }
+                { transform.Translate(Vector3.left * Time.deltaTime * turnSpeed * -1); 
+                }
             }
-            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.Space))
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Space))
             {
                 if (isJumping == false)
                 {
@@ -51,38 +44,26 @@ public class PlayerMove : MonoBehaviour
                     StartCoroutine(JumpSequence());
                 }
             }
-            {
+            
             }
 
 
-            if(isJumping == true)
+            if (isJumping == true)
             {
-                if(comingDown == false)
+                if (comingDown == false)
                 {
-                    transform.Translate(Vector3.up * Time.deltaTime * 4, Space.World);
+                    transform.Translate(Vector3.up * Time.deltaTime * 3, Space.World);
                 }
-                
-                if(comingDown == true)
+
+                if (comingDown == true)
                 {
-                    transform.Translate(Vector3.down * Time.deltaTime * 4, Space.World);
+                    transform.Translate(Vector3.down * Time.deltaTime * 3, Space.World);
                 }
-                
+
             }
-            /*  if (transform.position.x < LevelBoundary.leftside)
-              {
-                  transform.position = new Vector3(LevelBoundary.leftside, transform.position.y, transform.position.z);
-              }
-              if (transform.position.x > LevelBoundary.rightside)
-              {
-                  transform.position = new Vector3(LevelBoundary.rightside, transform.position.y, transform.position.z);
-              }
-            */
-
-
-
-
         }
-    }
+
+
     IEnumerator JumpSequence()
     {
         yield return new WaitForSeconds(0.45f);
@@ -90,5 +71,6 @@ public class PlayerMove : MonoBehaviour
         yield return new WaitForSeconds(0.45f);
         isJumping = false;
         comingDown = false;
+        playerObject.GetComponent<Animator>().Play("Standard Run");
     }
 }
